b222330ffb23eea7ad0dcfe9ed5246ef9116fa34
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-01-05 18:32:31 +1100

    Solutions for day 22 part 2

--- src/main/scala/com/ruchij/DayTwentyTwo.scala
+++ src/main/scala/com/ruchij/DayTwentyTwo.scala
@@ -3,16 +3,19 @@ package com.ruchij
 import cats.implicits._
 import com.ruchij.DayTwo.IntValue
 
+import scala.annotation.tailrec
 import scala.util.matching.Regex
 
 object DayTwentyTwo {
+
   case class Player(id: Int, cards: List[Int])
 
   object Player {
     val PlayerId: Regex = "Player (\\d+):".r
 
     def score(player: Player): Long =
-      List.range(1, player.cards.size + 1)
+      List
+        .range(1, player.cards.size + 1)
         .zip(player.cards.reverse)
         .map { case (multiplier, cardNumber) => (multiplier * cardNumber).toLong }
         .sum
@@ -21,8 +24,19 @@ object DayTwentyTwo {
   case class Game(playerOne: Player, playerTwo: Player)
 
   object Game {
-    def play(game: Game): Either[Player, Game] =
+    def playSingleRound(game: Game, playedGames: Set[Game]): Either[Player, Game] =
       (game.playerOne.cards, game.playerTwo.cards) match {
+        case (x :: xs, y :: _) if playedGames.contains(game) =>
+          Left(Player(game.playerOne.id, xs :+ x :+ y))
+
+        case (x :: xs, y :: ys) if x <= xs.size && y <= ys.size =>
+          val winner = playUntilWinner(Game(Player(game.playerOne.id, xs.take(x)), Player(game.playerTwo.id, ys.take(y))), Set.empty)
+
+          if (winner.id == game.playerOne.id)
+            Right(Game(Player(game.playerOne.id, xs :+ x :+ y), Player(game.playerTwo.id, ys)))
+          else
+            Right(Game(Player(game.playerOne.id, xs), Player(game.playerTwo.id, ys :+ y :+ x)))
+
         case (x :: xs, y :: ys) =>
           Right {
             if (x > y) Game(Player(game.playerOne.id, xs :+ x :+ y), Player(game.playerTwo.id, ys))
@@ -34,8 +48,12 @@ object DayTwentyTwo {
         case (_, Nil) => Left(game.playerOne)
       }
 
-    def playUntilWinner(game: Game): Player =
-      play(game).fold[Player](identity, playUntilWinner)
+    @tailrec
+    def playUntilWinner(game: Game, playedGames: Set[Game]): Player =
+      playSingleRound(game, playedGames) match {
+        case Left(player) => player
+        case Right(newGame) => playUntilWinner(newGame, playedGames + game)
+      }
   }
 
   def solve(input: List[String]) =
@@ -46,7 +64,7 @@ object DayTwentyTwo {
 
         case players => Left(s"Expected 2 players, but found ${players.size} players")
       }
-      .map(Game.playUntilWinner)
+      .map(game => Game.playUntilWinner(game, Set.empty))
       .map(Player.score)
 
   def split(input: List[String], condition: String => Boolean): List[List[String]] =
@@ -60,10 +78,11 @@ object DayTwentyTwo {
 
   val parsePlayer: List[String] => Either[String, Player] = {
     case Player.PlayerId(IntValue(id)) :: cardNumbers =>
-      cardNumbers.traverse { number =>
-        number.toIntOption.fold[Either[String, Int]](Left(s"""Unable to parse "$number" as a number"""))(Right.apply)
-      }
-      .map(cardNumbers => Player(id, cardNumbers))
+      cardNumbers
+        .traverse { number =>
+          number.toIntOption.fold[Either[String, Int]](Left(s"""Unable to parse "$number" as a number"""))(Right.apply)
+        }
+        .map(cardNumbers => Player(id, cardNumbers))
 
     case input => Left(s"""Unable to parse $input as a ${classOf[Player].getSimpleName}""")
   }
