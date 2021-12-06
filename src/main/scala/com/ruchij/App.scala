967eee961fccbf7506e113d92a98cef77c290a5c
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-12-06 17:09:13 +1100

    Solutions to Day 4 Part 1

--- src/main/scala/com/ruchij/App.scala
+++ src/main/scala/com/ruchij/App.scala
@@ -2,7 +2,7 @@ package com.ruchij
 
 import cats.effect.{ExitCode, IO, IOApp, Sync}
 import cats.implicits._
-import com.ruchij.twentytwentyone.{DayThree, DayTwo}
+import com.ruchij.twentytwentyone.{DayFour, DayThree, DayTwo}
 import fs2.Stream
 import fs2.io.file.{Files, Path}
 import fs2.text.lines
@@ -20,10 +20,10 @@ object App extends IOApp
   def solve[F[_]: Sync: Files] =
     for {
       path <- Sync[F].blocking {
-        Path.fromNioPath(Paths.get("./input/2021/day-3.txt"))
+        Path.fromNioPath(Paths.get("./input/2021/day-4.txt"))
       }
       inputData <- input[F](path).compile.toList
-      result = DayThree.solve(inputData)
+      result = DayFour.solve(inputData)
     }
     yield result
 
---
+++ src/main/scala/com/ruchij/twentytwentyone/DayFour.scala
@@ -0,0 +1,102 @@
+package com.ruchij.twentytwentyone
+
+import com.ruchij.twentytwentyone.Utils.IntValue
+
+object DayFour {
+  class BingoTile(val number: Int, var marked: Boolean) {
+    override def toString: String = s"$number"
+  }
+
+  case class BingoBoard(board: Seq[Seq[BingoTile]]) {
+    override def toString: String =
+      board.map(_.mkString(" ")).mkString("\n")
+
+    def mark(value: Int): Option[BingoTile] = {
+      val maybeTitle = board.flatten.find(tile => tile.number == value)
+
+      maybeTitle.foreach {
+        tile => tile.marked = true
+      }
+
+      maybeTitle
+    }
+  }
+
+  object BingoBoard {
+    def isWinner(bingoBoard: BingoBoard): Boolean = {
+      bingoBoard.board.exists { row => row.forall(tile => tile.marked) } ||
+        bingoBoard.board.indices.exists {
+          index =>
+            bingoBoard.board.forall { row => row(index).marked }
+        }
+    }
+
+    def unmarkedSum(bingoBoard: BingoBoard): Int =
+      bingoBoard.board.flatten
+        .filter(tile => !tile.marked)
+        .map(_.number)
+        .sum
+  }
+
+  case class Game(numbers: List[Int], boards: Seq[BingoBoard]) {
+    override def toString: String =
+      s"""
+         |${numbers.mkString(", ")}
+         |
+         |${boards.mkString("\n\n")}
+         |""".stripMargin
+  }
+
+  def solve(input: List[String]) =
+    parse(input).flatMap {
+      game => play(game)
+    }
+
+  def play(game: Game): Either[String, Int] =
+    game.numbers match {
+      case head :: tail =>
+        turn(head, game.boards) match {
+          case None => play(Game(tail, game.boards))
+
+          case Some(board) => Right(BingoBoard.unmarkedSum(board) * head)
+        }
+
+      case _ => Left("No winner")
+    }
+
+  def turn(number: Int, boards: Seq[BingoBoard]) = {
+    boards.foreach { board => board.mark(number) }
+
+    boards.find(BingoBoard.isWinner)
+  }
+
+  def parse(input: List[String]) =
+    input match {
+      case head :: tail =>
+        val numbers = head.split(',').map(_.trim).flatMap(_.toIntOption).toList
+        val boards = parseBoards(tail)
+
+        Right { Game(numbers, boards) }
+
+      case _ => Left("Empty input list")
+    }
+
+  def parseBoards(input: List[String]) =
+    input.filter(_.trim.nonEmpty)
+      .grouped(5)
+      .toList
+      .map {
+        boardRows =>
+          BingoBoard {
+            boardRows.map {
+              _.split(' ')
+                .collect { case IntValue(number) => number }
+                .map {
+                  value => new BingoTile(value, false)
+                }
+                .toList
+            }
+          }
+      }
+
+}
