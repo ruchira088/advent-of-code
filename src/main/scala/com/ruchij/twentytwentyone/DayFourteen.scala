1124c866e554b0f434998bda157132b2982a9b3f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-12-15 18:46:02 +1100

    Solutions to Day 14 Part 2

--- src/main/scala/com/ruchij/twentytwentyone/DayFourteen.scala
+++ src/main/scala/com/ruchij/twentytwentyone/DayFourteen.scala
@@ -9,15 +9,52 @@ object DayFourteen {
   def solve(input: List[String]) = {
     val puzzle = parse(input)
 
-    val result =
-      Range(0, 20).foldLeft(puzzle.start) {
-        case (acc, _) => runStep(acc, puzzle.mappings, List.empty)
+    val polymer: Map[Pair, Long] =
+      puzzle.start.zip(puzzle.start.tail)
+      .map { case (first, second) => Pair(first, second) }
+      .foldLeft(Map.empty[Pair, Long]) {
+        case (acc, pair) => acc ++ Map(pair -> (acc.getOrElse(pair, 0L) + 1))
       }
 
-    val counts = result.groupBy(identity).map { case (_, value) => value.size }
+    val result: Map[Pair, Long] =
+      Range(0, 40).foldLeft(polymer) {
+        case (acc, _) => runStep(acc, puzzle.mappings)
+      }
+
+    val borders = puzzle.start.headOption.toSet ++ puzzle.start.lastOption.toSet
+
+    val counts =
+      count(result)
+        .map { case (char, count) => if (borders.contains(char)) count + 1 else count }
 
     counts.max - counts.min
   }
+
+  def runStep(input: Map[Pair, Long], mappings: Map[Pair, Char]) =
+    input.foldLeft(Map.empty[Pair, Long]) {
+      case (acc, (pair, count)) =>
+        mappings.get(pair) match {
+          case None => acc ++ Map(pair -> (acc.getOrElse(pair, 0L) + count))
+          case Some(char) =>
+            acc ++
+              Map[Pair, Long](
+                Pair(pair.first, char) -> (acc.getOrElse(Pair(pair.first, char), 0L) + count),
+                Pair(char, pair.second) -> (acc.getOrElse(Pair(char, pair.second), 0L) + count)
+              )
+        }
+    }
+
+  def count(input: Map[Pair, Long]): Map[Char, Long] =
+    input.toList
+      .flatMap { case (Pair(first, second), count) => List(first -> count, second -> count) }
+      .foldLeft(Map.empty[Char, Long]) {
+        case (acc, (char, count)) =>
+          acc ++ Map(char -> (acc.getOrElse(char, 0L) + count))
+      }
+      .map {
+        case (char, count) => char -> count / 2
+      }
+
 
   def runStep(input: List[Char], mappings: Map[Pair, Char], result: List[Char]): List[Char] =
     input match {
