023bfd66ab52f845d29ee3f4901a9f416aa9c8f4
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-12-19 22:02:26 +1100

    Solutions for Day 18 Part 1

--- src/main/scala/com/ruchij/twentytwentyone/DayEighteen.scala
+++ src/main/scala/com/ruchij/twentytwentyone/DayEighteen.scala
@@ -3,72 +3,197 @@ package com.ruchij.twentytwentyone
 import scala.annotation.tailrec
 
 object DayEighteen {
-  sealed trait SnailNumber {}
+  sealed trait SnailNumber {
+    var parent: Option[SnailNumber.Pair]
+  }
 
   object SnailNumber {
-    case class Regular(value: Int) extends SnailNumber {
+    case class Regular(var value: Int, var parent: Option[SnailNumber.Pair]) extends SnailNumber {
       override def toString: String = value.toString
     }
 
-    case class Pair(left: SnailNumber, right: SnailNumber) extends SnailNumber {
+    case class Pair(var left: SnailNumber, var right: SnailNumber, var parent: Option[SnailNumber.Pair])
+        extends SnailNumber {
       override def toString: String = s"[$left,$right]"
     }
   }
 
-  case class ParseResult[A, B](value: A, remaining: B)
-
   def solve(input: List[String]) = {
-    val data = "[[[[0,2],4],[7,[[8,4],9]]],[1,1]]"
+    val snailNumber =
+      input
+        .map(line => parse(line, None))
+        .reduce {
+          (x, y) =>
+            val pair = SnailNumber.Pair(x, y, None)
+            x.parent = Some(pair)
+            y.parent = Some(pair)
 
-    val snailNumber = parse(data)
+            reduce(pair)
+        }
 
-    nested(snailNumber, 4)
+    magnitude(snailNumber)
   }
 
-  def parse(input: String): SnailNumber =
-    if (input.startsWith("[")) parsePair(input, 0, "")
-    else SnailNumber.Regular(input.toInt)
+  def reduce(snailNumber: SnailNumber): SnailNumber =
+    oneStep(snailNumber) match {
+      case None => snailNumber
+      case Some(value) => reduce(value)
+    }
+
+  def oneStep(snailNumber: SnailNumber): Option[SnailNumber] =
+    updatedNested(snailNumber).orElse(updateHighest(snailNumber))
+
+  def updateHighest(snailNumber: SnailNumber): Option[SnailNumber] = {
+    val result = findHigh(snailNumber, 9)
+
+    result.foreach {
+      case number @ SnailNumber.Regular(value, Some(parent)) =>
+        val split =
+          SnailNumber.Pair(
+            SnailNumber.Regular(math.floor(value.toDouble / 2).toInt, None),
+            SnailNumber.Regular(math.ceil(value.toDouble / 2).toInt, None),
+            Some(parent)
+          )
+
+        split.right.parent = Some(split)
+        split.left.parent = Some(split)
+
+        if (parent.left.eq(number)) {
+          parent.left = split
+        } else {
+          parent.right = split
+        }
+    }
+
+    result.map(_ => snailNumber)
+  }
+
+  def updatedNested(snailNumber: SnailNumber): Option[SnailNumber] = {
+    val result = findNested(snailNumber, 4)
+
+    result.foreach {
+      case number @ SnailNumber.Pair(SnailNumber.Regular(left, _), SnailNumber.Regular(right, _), Some(parent)) =>
+        leftNeighbour(number, None)
+          .foreach { regular =>
+            regular.value = regular.value + left
+          }
+
+        rightNeighbour(number, None)
+          .foreach { regular =>
+            regular.value = regular.value + right
+          }
+
+        if (parent.left == number) {
+          parent.left = SnailNumber.Regular(0, Some(parent))
+        } else {
+          parent.right = SnailNumber.Regular(0, Some(parent))
+        }
+
+        Some()
+
+      case _ =>
+        None
+    }
+
+    result.map(_ => snailNumber)
+  }
+
+  def rightMost(snailNumber: SnailNumber): Option[SnailNumber.Regular] =
+    snailNumber match {
+      case regular: SnailNumber.Regular => Some(regular)
+      case SnailNumber.Pair(_, right, _) => rightMost(right)
+    }
+
+  def leftMost(snailNumber: SnailNumber): Option[SnailNumber.Regular] =
+    snailNumber match {
+      case regular: SnailNumber.Regular => Some(regular)
+      case SnailNumber.Pair(left, _, _) => leftMost(left)
+    }
+
+  def leftNeighbour(snailNumber: SnailNumber, last: Option[SnailNumber]): Option[SnailNumber.Regular] =
+    snailNumber match {
+      case _ if last.isEmpty && snailNumber.parent.nonEmpty =>
+        leftNeighbour(snailNumber.parent.get, Some(snailNumber))
+
+      case SnailNumber.Pair(left, _, _) if last.exists(x => !x.eq(left)) =>
+        rightMost(left)
+
+      case regular: SnailNumber.Regular => Some(regular)
+
+      case SnailNumber.Pair(_, _, Some(parent)) =>
+        leftNeighbour(parent, Some(snailNumber))
+
+      case _ => None
+    }
+
+  def rightNeighbour(snailNumber: SnailNumber, last: Option[SnailNumber]): Option[SnailNumber.Regular] =
+    snailNumber match {
+      case _ if last.isEmpty && snailNumber.parent.nonEmpty =>
+        rightNeighbour(snailNumber.parent.get, Some(snailNumber))
+
+      case SnailNumber.Pair(_, right, _) if last.exists(x => !x.eq(right)) =>
+        leftMost(right)
+
+      case regular: SnailNumber.Regular => Some(regular)
+
+      case SnailNumber.Pair(_, _, Some(parent)) =>
+        rightNeighbour(parent, Some(snailNumber))
+
+      case _ => None
+    }
+
+  def parse(input: String, parent: Option[SnailNumber.Pair]): SnailNumber =
+    if (input.startsWith("[")) parsePair(input, 0, "", parent)
+    else SnailNumber.Regular(input.toInt, parent)
 
   @tailrec
-  def parsePair(input: String, bracketOffset: Int, acc: String): SnailNumber.Pair =
+  def parsePair(input: String, bracketOffset: Int, acc: String, parent: Option[SnailNumber.Pair]): SnailNumber.Pair =
     input.toList match {
       case '[' :: rest =>
-        parsePair(rest.mkString, bracketOffset + 1, acc + "[")
+        parsePair(rest.mkString, bracketOffset + 1, acc + "[", parent)
 
       case ']' :: rest =>
-        parsePair(rest.mkString, bracketOffset - 1, acc + "]")
+        parsePair(rest.mkString, bracketOffset - 1, acc + "]", parent)
 
       case ',' :: rest if bracketOffset == 1 =>
-        SnailNumber.Pair(parse(acc.tail), parse(rest.init.mkString))
+        val left = parse(acc.tail, None)
+        val right = parse(rest.init.mkString, None)
+
+        val number: SnailNumber.Pair =
+          SnailNumber.Pair(left, right, parent)
+
+        left.parent = Some(number)
+        right.parent = Some(number)
+
+        number
 
       case head :: tail =>
-        parsePair(tail.mkString, bracketOffset, acc + head)
+        parsePair(tail.mkString, bracketOffset, acc + head, parent)
     }
 
-  def findNestedIndex(input: String, bracketOffset: Int, index: Int): Option[Int] =
-    input.headOption match {
-      case Some('[') if bracketOffset == 4 => Some(index)
-      case Some('[') => findNestedIndex(input.tail, bracketOffset + 1, index + 1)
-      case Some(']') => findNestedIndex(input.tail, bracketOffset - 1, index + 1)
-      case Some(_) => findNestedIndex(input.tail, bracketOffset, index + 1)
-      case None => None
-    }
-
-  def nested(snailNumber: SnailNumber, level: Int): Option[SnailNumber.Pair] =
+  def findNested(snailNumber: SnailNumber, level: Int): Option[SnailNumber.Pair] =
     snailNumber match {
-      case SnailNumber.Regular(_) => None
+      case SnailNumber.Regular(_, _) => None
 
-      case pair @ SnailNumber.Pair(_, _) if level == 0 => Some(pair)
+      case pair @ SnailNumber.Pair(_, _, _) if level == 0 => Some(pair)
 
-      case SnailNumber.Pair(left, right) =>
-        nested(left, level - 1).orElse(nested(right, level - 1))
+      case SnailNumber.Pair(left, right, _) =>
+        findNested(left, level - 1).orElse(findNested(right, level - 1))
     }
 
-  def explode(input: String) =
-    findNestedIndex(input, 0, 0)
-      .map {
-        index =>
+  def findHigh(snailNumber: SnailNumber, max: Int): Option[SnailNumber.Regular] =
+    snailNumber match {
+      case regular @ SnailNumber.Regular(value, _) if value > max => Some(regular)
+      case _: SnailNumber.Regular => None
+      case SnailNumber.Pair(left, right, _) =>
+        findHigh(left, max).orElse(findHigh(right, max))
+    }
 
-      }
+  def magnitude(snailNumber: SnailNumber): Int =
+    snailNumber match {
+      case SnailNumber.Regular(value, _) => value
+      case SnailNumber.Pair(left, right, _) =>
+        magnitude(left) * 3 + magnitude(right) * 2
+    }
 
 }
