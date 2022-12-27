7f18ff1e31a02206f771725fb1c0361802d79c00
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-12-28 01:56:35 +1100

    Solutions for day 16 part 2

--- src/main/java/com/ruchij/twentytwentytwo/DaySixteen.java
+++ src/main/java/com/ruchij/twentytwentytwo/DaySixteen.java
@@ -2,6 +2,7 @@ package com.ruchij.twentytwentytwo;
 
 import com.ruchij.JavaSolution;
 
+import java.time.Instant;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,54 +13,100 @@ public class DaySixteen implements JavaSolution {
     record Valve(String id, int flowRate, Set<String> connections) {
     }
 
-    record Entry(String valveId, Set<String> openedValves, int timestamp, int pressure) {
+    record Entry(String myValveId, String elephantValveId, Set<String> openedValves,
+                 int myTimestamp,
+                 int elephantTimestamp, int pressure) {
     }
 
-    record Visit(String position, Set<String> openedValves, int pressure) {
+    record Visit(String myValveId, String elephantValveId, int pressure) {
     }
 
     @Override
     public Object solve(Stream<String> input) {
-        Map<String, Valve> valves = parse(input);
-        Set<String> noZeroValves =
-                valves.values().stream()
-                        .filter(valve -> valve.flowRate != 0)
-                        .map(Valve::id)
-                        .collect(Collectors.toSet());
+        Instant start = Instant.now();
 
+        Map<String, Valve> valves = parse(input);
         ArrayDeque<Entry> entries = new ArrayDeque<>();
-        Entry seed = new Entry("AA", new HashSet<>(), 30, 0);
+        Entry seed = new Entry("AA", "AA", new HashSet<>(), 26, 26, 0);
         entries.add(seed);
 
         HashSet<Visit> visited = new HashSet<>();
 
-        long max = 0;
+        int max = 0;
 
         while (!entries.isEmpty()) {
             Entry entry = entries.poll();
-            Valve valve = valves.get(entry.valveId);
+            Valve myValve = valves.get(entry.myValveId);
+            Valve elephantValve = valves.get(entry.elephantValveId);
 
-            Visit visit = new Visit(entry.valveId, entry.openedValves, entry.pressure);
+            Visit visit = new Visit(entry.myValveId, entry.elephantValveId, entry.pressure);
 
             if (max < entry.pressure) {
                 max = entry.pressure;
+                System.out.println(max);
             }
 
-            if (!visited.contains(visit) && !entry.openedValves.containsAll(noZeroValves)) {
+
+            if (!visited.contains(visit)) {
                 visited.add(visit);
 
-                if (entry.timestamp > 1) {
-                    boolean shouldOpenValve = valve.flowRate > 0 && !entry.openedValves.contains(valve.id);
-                    HashSet<String> openedValves = new HashSet<>(entry.openedValves);
-                    openedValves.add(valve.id);
+                if (entry.myTimestamp > 1 || entry.elephantTimestamp > 1) {
+                    boolean shouldOpenMyValve = myValve.flowRate > 0 && entry.myTimestamp > 1 && !entry.openedValves.contains(myValve.id);
+                    boolean shouldOpenElephantValve = elephantValve.flowRate > 0 && entry.elephantTimestamp > 1 && !entry.openedValves.contains(elephantValve.id);
 
-                    for (String next : valve.connections) {
-                        Entry notOpeningValve = new Entry(next, entry.openedValves, entry.timestamp - 1, entry.pressure);
-                        entries.add(notOpeningValve);
+                    for (String myNext : myValve.connections) {
+                        for (String elephantNext : elephantValve.connections) {
+                            if (entry.myTimestamp > 1 && entry.elephantTimestamp > 1) {
+                                Entry next = new Entry(myNext, elephantNext, entry.openedValves, entry.myTimestamp - 1, entry.elephantTimestamp - 1, entry.pressure);
+                                entries.add(next);
 
-                        if (shouldOpenValve) {
-                            Entry openingValve = new Entry(next, openedValves, entry.timestamp - 2, entry.pressure + (entry.timestamp - 1) * valve.flowRate);
-                            entries.add(openingValve);
+                                if (shouldOpenElephantValve && shouldOpenMyValve && !myValve.id.equals(elephantValve.id)) {
+                                    HashSet<String> openedValves = new HashSet<>(entry.openedValves);
+                                    openedValves.add(myValve.id);
+                                    openedValves.add(elephantValve.id);
+
+                                    Entry nextEntry = new Entry(myNext, elephantNext, openedValves, entry.myTimestamp - 2, entry.elephantTimestamp - 2, entry.pressure + (entry.myTimestamp - 1) * myValve.flowRate + (entry.elephantTimestamp - 1) * elephantValve.flowRate);
+                                    entries.add(nextEntry);
+                                }
+
+                                if (shouldOpenMyValve) {
+                                    HashSet<String> openedValves = new HashSet<>(entry.openedValves);
+                                    openedValves.add(myValve.id);
+
+                                    Entry nextEntry = new Entry(myNext, elephantNext, openedValves, entry.myTimestamp - 2, entry.elephantTimestamp - 1, entry.pressure + (entry.myTimestamp - 1) * myValve.flowRate);
+                                    entries.add(nextEntry);
+                                }
+
+                                if (shouldOpenElephantValve) {
+                                    HashSet<String> openedValves = new HashSet<>(entry.openedValves);
+                                    openedValves.add(elephantValve.id);
+
+                                    Entry nextEntry = new Entry(myNext, elephantNext, openedValves, entry.myTimestamp - 1, entry.elephantTimestamp - 2, entry.pressure + (entry.elephantTimestamp - 1) * elephantValve.flowRate);
+                                    entries.add(nextEntry);
+                                }
+                            } else if (entry.myTimestamp > 1) {
+                                Entry next = new Entry(myNext, elephantValve.id, entry.openedValves, entry.myTimestamp - 1, entry.elephantTimestamp, entry.pressure);
+                                entries.add(next);
+
+                                if (shouldOpenMyValve) {
+                                    HashSet<String> openedValves = new HashSet<>(entry.openedValves);
+                                    openedValves.add(myValve.id);
+
+                                    Entry openingValve = new Entry(myNext, elephantValve.id, openedValves, entry.myTimestamp - 2, entry.elephantTimestamp, entry.pressure + (entry.myTimestamp - 1) * myValve.flowRate);
+                                    entries.add(openingValve);
+                                }
+                            } else {
+                                Entry next = new Entry(myValve.id, elephantNext, entry.openedValves, entry.myTimestamp, entry.elephantTimestamp - 1, entry.pressure);
+                                entries.add(next);
+
+                                if (shouldOpenElephantValve) {
+                                    HashSet<String> openedValves = new HashSet<>(entry.openedValves);
+                                    openedValves.add(elephantValve.id);
+
+                                    Entry openingValve = new Entry(myValve.id, elephantNext, openedValves, entry.myTimestamp, entry.elephantTimestamp - 2, entry.pressure + (entry.elephantTimestamp - 1) * elephantValve.flowRate);
+                                    entries.add(openingValve);
+                                }
+                            }
                         }
                     }
                 }
@@ -68,6 +115,8 @@ public class DaySixteen implements JavaSolution {
 
         // AA -> DD -> CC -> BB -> AA -> II -> JJ -> II -> AA -> DD -> EE -> FF -> GG -> HH -> GG -> FF -> EE -> DD -> CC
         // 1651
+
+        System.out.println("Duration: %sms".formatted(Instant.now().toEpochMilli() - start.toEpochMilli()));
 
         return max;
     }
