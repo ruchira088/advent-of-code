a25aa1dfa2684c56957b12d7c46f2876b4436f23
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-03-15 21:12:06 +1100

    Solution 2 for Day 6 2023

--- src/main/java/com/ruchij/twentytwentythree/DaySix.java
+++ src/main/java/com/ruchij/twentytwentythree/DaySix.java
@@ -2,42 +2,25 @@ package com.ruchij.twentytwentythree;
 
 import com.ruchij.JavaSolution;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 public class DaySix implements JavaSolution {
-    record Race(int time, int record) {
+    record Race(long time, long record) {
     }
 
     @Override
     public Object solve(Stream<String> __) {
-        List<Race> races =
-                List.of(
-//                        new Race(7, 9),
-//                        new Race(15, 40),
-//                        new Race(30, 200)
-                        new Race(55, 401),
-                        new Race(99, 1485),
-                        new Race(97, 2274),
-                        new Race(93, 1405)
-
-                );
-
-        long result = 1;
-
-        for (Race race : races) {
-            result = result * distances(race);
-        }
-
-        return result;
+//        Race race = new Race(71530, 940200);
+        Race race = new Race(55999793, 401148522741405L);
+        return distances(race);
     }
 
-    int distances(Race race) {
-        int count = 0;
+    long distances(Race race) {
+        long count = 0;
 
-        for (int i = 0; i <= race.time; i++) {
-            int speed = i;
-            int distance = speed * (race.time - i);
+        for (long i = 0; i <= race.time; i++) {
+            long speed = i;
+            long distance = speed * (race.time - i);
 
             if (distance > race.record) {
                 count++;
